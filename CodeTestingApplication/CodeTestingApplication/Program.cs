f0217f51051c265650b4186186fdd1d28c60db89
michaelarellanotech <michael.arellano.tech@gmail.com>
2014-03-25 06:50:32 +1100

    Commit

---
+++ CodeTestingApplication/CodeTestingApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeTestingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic test = "test";
+
+            test += " 123";
+
+            Console.WriteLine(test);
+
+            test = 1;
+
+            test += 1;
+
+            Console.WriteLine(test);
+
+            test += "23";
+
+            Console.WriteLine(test);
+
+            test = Convert.ToInt32(test);
+
+            Console.WriteLine(test);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
